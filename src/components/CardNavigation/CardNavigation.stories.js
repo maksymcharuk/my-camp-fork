5f26be314a263ac9ab7a810691b21576f3f3275c
Maksym Charuk <maxcharuk@gmail.com>
2022-09-22 23:44:14 -0400

    Setup react Storybook

--- src/components/CardNavigation/CardNavigation.stories.js
+++ src/components/CardNavigation/CardNavigation.stories.js
@@ -1,12 +1,20 @@
 /* eslint-disable */
-import CardNavigation from './CardNavigation';
+import CardNavigation from "./CardNavigation";
 
 export default {
   title: "CardNavigation",
 };
 
-export const Default = () => <CardNavigation />;
+export const Default = () => (
+  <CardNavigation
+    type="wide"
+    iconSrc="https://assets.website-files.com/62e852b3b3432f63a22b8844/62e8ea7a1e7c87282c7450b6_streamlinehq-interface-content-book-open-interface-essential-250.SVG"
+    title="Camp Program"
+    titleSize="large"
+    description="From wake-up to sleep"
+  />
+);
 
 Default.story = {
-  name: 'default',
+  name: "default",
 };
--- src/components/PageTitle/PageTitle.stories.js
+++ src/components/PageTitle/PageTitle.stories.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
-import PageTitle from './PageTitle';
+import PageTitle from "./PageTitle";
 
 export default {
   title: "PageTitle",
+  component: PageTitle,
 };
 
-export const Default = () => <PageTitle />;
+export const Default = () => (
+  <PageTitle title="Summer Camp" description="Open air Camp for kids" />
+);
 
 Default.story = {
-  name: 'default',
+  name: "default",
 };
